c61156d86bc702d8dcf18b69e47bc2279d0c5158
AliSubhan786 <47426993+AliSubhan786@users.noreply.github.com>
2026-08-18 03:32:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("EV Markez website loaded");
+const menuToggle = document.getElementById("menuToggle");
+const nav = document.getElementById("nav");
+
+menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+const navLinks = document.querySelectorAll(".nav a");
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+});
+
+document.getElementById("year").textContent =
+    new Date().getFullYear();
